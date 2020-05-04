b1fd0711a83963da5ea5428d0f8329be50207a0c
rudy <rudy.vissers@m_team.be>
2020-05-04 11:41:23 +0200

    testing Java Files

---
+++ src/test/java/api/java/nio/file/FilesTest.java
@@ -0,0 +1,24 @@
+package api.java.nio.file;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+public class FilesTest {
+
+    @Test
+    public void whenCreateTempFile_thenTempFileIsCreated() {
+        try {
+            Path tempFile = Files.createTempFile(null, ".myapp");
+            // /tmp/1063942235502049642.myapp
+            assertThat(tempFile).exists();
+        } catch (IOException e) {
+            fail("It was not pssible to create the temp file!");
+        }
+    }
+}

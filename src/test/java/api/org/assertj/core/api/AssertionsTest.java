7deb3d64c23244f41fb1485ede88514fd6b4f913
rudy <rudy.vissers@m_team.be>
2020-05-06 09:08:08 +0200

    AssertJ assertions

--- src/test/java/api/org/assertj/core/api/AssertionsTest.java
+++ src/test/java/api/org/assertj/core/api/AssertionsTest.java
@@ -3,34 +3,146 @@ package api.org.assertj.core.api;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class AssertionsTest {
 
-    Character frodo;
+    private TolkienCharacter frodo;
+    private TolkienCharacter sauron;
+    private TolkienCharacter sam;
+    private TolkienCharacter boromir;
+    private TolkienCharacter legolas;
+    private TolkienCharacter aragorn;
+    private List<TolkienCharacter> fellowshipOfTheRing;
 
     @Before
     public void setUp() throws Exception {
-        frodo = new Character("Frodo");
+        frodo = new TolkienCharacter("Frodo", 32, new Race("Hobbit"));
+        sauron = new TolkienCharacter("Sauron", 33, new Race("Elf"));
+        sam = new TolkienCharacter("Sam", 38, new Race("Hobbit"));
+        boromir = new TolkienCharacter("Boromir", 37, new Race("Man"));
+        legolas = new TolkienCharacter("Legolas", 1000, new Race("Elf"));
+        aragorn = new TolkienCharacter("Aragorn", 1000, new Race("Elf"));
+        fellowshipOfTheRing = Arrays.asList(frodo, sam, boromir, legolas, aragorn);
     }
 
     @Test
     public void assertThatTest() {
+
         assertThat(frodo.getName()).isEqualTo("Frodo");
+
+        assertThat(frodo).isNotEqualTo(sauron);
+
+        assertThat(frodo.getName()).startsWith("Fro")
+                .endsWith("do")
+                .isEqualToIgnoringCase("frodo");
+
+        assertThat(fellowshipOfTheRing).hasSize(5)
+                .contains(frodo, sam)
+                .doesNotContain(sauron);
+
+        assertThat(frodo.getAge())
+                .as("check %s's age", frodo.getName())
+                .isEqualTo(32);
+
+        assertThat(fellowshipOfTheRing)
+                .extracting(TolkienCharacter::getName)
+                .doesNotContain("Sauron", "Elrond");
+
+        assertThat(fellowshipOfTheRing)
+                .extracting("name", "age", "race.name")
+                .contains(tuple("Boromir", 37, "Man"),
+                        tuple("Sam", 38, "Hobbit"),
+                        tuple("Legolas", 1000, "Elf"));
+
+        assertThat(fellowshipOfTheRing)
+                .filteredOn(character -> character.getName().contains("o"))
+                .containsOnly(aragorn, frodo, legolas, boromir);
+
+        assertThat(fellowshipOfTheRing)
+                .filteredOn(character -> character.getName().contains("g"))
+                .containsOnly(aragorn, legolas);
+
+        assertThat(fellowshipOfTheRing)
+                .filteredOn(character -> character.getName().contains("o"))
+                .containsOnly(aragorn, frodo, legolas, boromir)
+                .extracting(character -> character.getRace().getName())
+                .contains("Hobbit", "Elf", "Man");
+    }
+
+    @Test
+    public void assertThatThrownByTest() {
+        assertThatThrownBy(() -> { throw new Exception("boom!"); })
+                .hasMessage("boom!");
+    }
+
+    @Test
+    public void catchTrowableTest() {
+        Throwable thrown = catchThrowable(() -> { throw new Exception("boom!"); });
+        assertThat(thrown).hasMessageContaining("boom");
     }
 }
 
-class Character {
+// FIXME: 6/05/20 make usage of Enum
+class Race {
+
     String name;
+
+    Race(String name) {
+        this.name = name;
+    }
 
     String getName() {
         return name;
     }
 
-    Character(String name) {
+    void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Race{" + "name='" + name + '\'' + '}';
+    }
+}
+
+class TolkienCharacter {
+    String name;
+    int age;
+    Race race;
+
+    TolkienCharacter(String name, int age, Race race) {
+        this.name = name;
+        this.age = age;
+        this.race = race;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    void setAge(int age) {
+        this.age = age;
+    }
+
+    Race getRace() {
+        return race;
+    }
+
+    void setRace(Race race) {
+        this.race = race;
     }
 
     @Override
@@ -41,12 +153,17 @@ class Character {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        Character character = (Character) o;
-        return name.equals(character.name);
+        TolkienCharacter character = (TolkienCharacter) o;
+        return age == character.age && name.equals(character.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "TolkienCharacter{" + "name='" + name + '\'' + ", age=" + age + ", race=" + race + '}';
     }
 }

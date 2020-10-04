cb4f6573338a6b071b40e2008dcddcb4ab680546
Rudy Vissers <rudy@rudyvissers.com>
2020-10-04 21:14:59 +0200

    indentation

--- src/test/java/api/com/google/common/base/FunctionTest.java
+++ src/test/java/api/com/google/common/base/FunctionTest.java
@@ -13,16 +13,17 @@ public class FunctionTest {
     public void givenFunctionOnDateAndString_thenReturnString() {
 
         // by Google Guava
-        com.google.common.base.Function<Date, String> functionGuava = dateGuava -> new SimpleDateFormat("dd/MM/yyyy").format(dateGuava);
+        com.google.common.base.Function<Date, String> functionGuava
+                = dateGuava -> new SimpleDateFormat("dd/MM/yyyy").format(dateGuava);
         Date date = new Date(1585031976022L);
         String simpleDateFormatGuava = functionGuava.apply(date);
         assertThat(simpleDateFormatGuava).isEqualTo("24/03/2020");
 
         // by Java
-        java.util.function.Function<Date, String> functionJava = dateJava -> new SimpleDateFormat("dd/MM/yyyy").format(dateJava);
+        java.util.function.Function<Date, String> functionJava
+                = dateJava -> new SimpleDateFormat("dd/MM/yyyy").format(dateJava);
         String simpleDateFormatJava = functionJava.apply(date);
         assertThat(simpleDateFormatJava).isEqualTo("24/03/2020");
-
     }
 
 }

fcffbaf15b9b409f089cbcad7ed08e30d6cdedd0
Rudy Vissers <rudy@rudyvissers.com>
2020-10-05 06:00:28 +0200

    imports were optimized

--- src/test/java/api/java/util/function/BiFunctionTest.java
+++ src/test/java/api/java/util/function/BiFunctionTest.java
@@ -12,10 +12,9 @@ public class BiFunctionTest {
 
     @Test
     public void givenBiFunctionSumIntegers_thenReturnSumOfIntegers() {
-        // Look at the syntax of a BiFunction.
-        // There are 3 parameters.
-        // 2 input parameters and 1 output parameter.
         BiFunction<Integer, Integer, Integer> biFunction = (i1, i2) -> i1 + i2;
+        // the generic type has 2 input, T and U and 1 output R
+        // R = apply(T,U)
         assertThat(biFunction.apply(1, 2)).isEqualTo(3);
     }
 }

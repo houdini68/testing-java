8b659c4749a71dd0ca750594cbc00daa9b833ad2
m999rvi <rudy.vissers@m-team.be>
2021-11-12 09:51:51 +0100

    better comment

--- src/main/java/api/java/lang/ShutdownHookDemo.java
+++ src/main/java/api/java/lang/ShutdownHookDemo.java
@@ -2,18 +2,18 @@ package api.java.lang;
 
 public class ShutdownHookDemo {
 
-    // a class that extends thread is to be called when the program does exit
-    static class ShutDownHook extends Thread {
-        public void run() {
-            System.out.println("This thread part of the class ShutDownHook is executed after the main thread exited...");
-        }
-    }
-
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Demo of a Shutdown hook.");
         Runtime.getRuntime().addShutdownHook(new ShutDownHook());
         System.out.println("Waiting 5 second...");
         Thread.sleep(5000);
         System.out.println("At the end of the main thread, the shutdownhook is executed");
+    }
+
+    // a class that extends thread is to be called when the program does exit
+    static class ShutDownHook extends Thread {
+        public void run() {
+            System.out.println("This thread, part of the class ShutDownHook, is executed after the main thread exited...");
+        }
     }
 }

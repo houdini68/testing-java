9dc067ed0ea6d9d3d71e19f8e44d643373e490d9
m999rvi <rudy.vissers@m-team.be>
2021-11-12 10:17:05 +0100

    exid is a customer concept and was renamed to id

--- src/test/java/api/java/lang/StringTest.java
+++ src/test/java/api/java/lang/StringTest.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 
 public class StringTest {
 
-    public static final String EXID_ON_12 = "830012918511";
+    public static final String ID_ON_12 = "830012918511";
 
     @Test
     public void givenIntegerString_whenLeftPaddingWith0SizeIs13_thenPaddTo13() {
-        String exid = EXID_ON_12;
-        Assertions.assertThat(String.format("%013d", Long.valueOf(exid))).isEqualTo("0" + EXID_ON_12);
+        String exid = ID_ON_12;
+        Assertions.assertThat(String.format("%013d", Long.valueOf(exid))).isEqualTo("0" + ID_ON_12);
     }
 }
